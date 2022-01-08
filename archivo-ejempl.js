d45602cd3af3700f098c164d727aa84f105c6931
oswaldo <oswaldo.perozo@gmail.com>
2022-01-08 00:55:39 -0300

    Modificado agregado 3

--- archivo-ejempl.js
+++ archivo-ejempl.js
@@ -12,7 +12,7 @@ const nuevoBool = null;
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
 
-// Resuelve el siguiente problema matemático:
+// Resuelve el siguiente problema matemnático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
